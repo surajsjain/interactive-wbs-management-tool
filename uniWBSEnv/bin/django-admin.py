ab9c009c123a59c1cef07e12b8ef40e153ce9430
Suraj S Jain <surajsjain@hotmail.com>
2019-09-10 09:40:48 +0530

    Added a virtual Environment for Django

---
+++ uniWBSEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/suraj/Work/Hackathons-And-Challenges/Unilever2/Final/unilever-wbs-management/uniWBSEnv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
