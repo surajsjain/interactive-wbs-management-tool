1851d56430ccdbfee83b837202976ab7325ac8ea
Suraj S Jain <surajsjain@hotmail.com>
2019-09-10 23:33:58 +0530

    fixed the issue with whitenoise

--- WBS_Management_Tool/settings.py
+++ WBS_Management_Tool/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
 ]
 
 ROOT_URLCONF = 'WBS_Management_Tool.urls'
